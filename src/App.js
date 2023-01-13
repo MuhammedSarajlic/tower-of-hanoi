8c0acbb14d6dab5895f02747c8afdcca387d0fc1
MuhammedSarajlic <muhammed.sarajlic.20@size.ba>
2023-01-13 01:19:21 +0100

    Move disc from one tower to another
    -Only move disc if disc is smaller than first disc on selected tower
    -Display "You win" text if user moves all discs to the last tower

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const DISC_NUM = 3;
+  const [towers, setTowers] = useState([[1, 2, 3], [], []]);
+  const [selectedTowerIdx, setSelectedTowerIdx] = useState();
+  const [numberOfMoves, setNumberOfmoves] = useState(0);
+
+  const handleSelectTower = (towerIdx) => {
+    if (selectedTowerIdx !== undefined) {
+      const tempTowers = [...towers];
+      if (tempTowers[selectedTowerIdx][0] < (tempTowers[towerIdx][0] ?? 9)) {
+        const poppedDisc = tempTowers[selectedTowerIdx].shift();
+        tempTowers[towerIdx].unshift(poppedDisc);
+        setTowers(tempTowers);
+        setNumberOfmoves((prev) => prev + 1);
+      }
+      setSelectedTowerIdx(undefined);
+    } else {
+      setSelectedTowerIdx(towerIdx);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+      {towers[2].length >= DISC_NUM && <div className="win">You win</div>}
+      <p className="moves">Number of moves: {numberOfMoves}</p>
+      {towers.map((tower, i) => (
+        <div
+          className="tower-space"
+          key={i}
+          onClick={() => handleSelectTower(i)}
         >
-          Learn React
-        </a>
-      </header>
+          <div
+            className="tower"
+            style={{ backgroundColor: selectedTowerIdx === i && "red" }}
+          >
+            <div className="discs">
+              {tower.map((disc, i) => (
+                <div
+                  className="disc"
+                  key={i}
+                  style={{ width: `${disc * 20 + 10}px` }}
+                ></div>
+              ))}
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
